48a06ed593d99ced5acae74729354b5522a4dcf2
Aaron Santamaria <aasantamariac@hotmail.es>
2022-10-13 00:08:28 -0600

    primer commit

---
+++ pages/login.py
@@ -0,0 +1,14 @@
+
+
+class inside_login_page:
+    def login_page():
+        pass
+
+    URL = 'https://d36ayf7y0dxdzq.cloudfront.net/login'
+
+class browser_class:
+    def __init__(self, browser):
+        self.browser = browser
+        
+    def load_page():
+         
---
+++ tests/test_inside.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+def test_prueba():
+    assert True
+
+    driver = webdriver.Firefox()
+    driver.get("http://www.python.org")
+    
