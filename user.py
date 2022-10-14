f8586d712f51369d7e82c8f7a3b17ef5a83f8fff
kurosakyth <aasantamariac@hotmail.es>
2022-10-13 19:58:28 -0600

    deleting user

--- user.py
+++ user.py
@@ -1,14 +1,2 @@
-# class user:
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
-
-#     def username_return(self):
-#         username= 'asantamaria@multiplied.com'
-#         return username
-
-#     def password_return(self):
-#         password = 'qwrt7890'
-#         return password
-username= 'asantamaria@multiplied.com'
-password = 'qwrt7890'
+username= '.'
+password = '.'
