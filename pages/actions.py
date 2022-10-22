6ffab799d7f8c3492b24af78b408aca71b3a5637
kurosakyth <aasantamariac@hotmail.es>
2022-10-22 15:34:50 -0600

    añadiendo tiempo

--- pages/actions.py
+++ pages/actions.py
@@ -18,26 +18,26 @@ class inside_login_page:
         return self.driver.title
 
     # Method to click / select a button / option on the page.
-    def click_btn(self, selector, timeout = 20):
+    def click_btn(self, selector, timeout = 30):
         wait = WebDriverWait(self.driver,timeout)
         btn = wait.until(ec.visibility_of_element_located(selector))
         btn.click()
 
     # Method to write on a textbox on the page.
-    def write_on_textbox(self, data_from_user, selector, timeout = 20):
+    def write_on_textbox(self, data_from_user, selector, timeout = 30):
         wait = WebDriverWait(self.driver, timeout)
         textbox = wait.until(ec.visibility_of_element_located(selector))
         textbox.send_keys(data_from_user + Keys.RETURN)
 
     # Method to find the texbox on the page.
-    def get_text_from_textbox(self,selector, timeout = 20):
+    def get_text_from_textbox(self,selector, timeout = 30):
         wait = WebDriverWait(self.driver, timeout)
         textbox = wait.until(ec.visibility_of_element_located(selector))
         textbox = textbox.get_attribute('value')
         return textbox
 
     # Method to get text. Used to get the alert on the timesheet.
-    def get_text(self,*selector, timeout = 20):
+    def get_text(self,*selector, timeout = 30):
         wait = WebDriverWait(self.driver, timeout)
         obj = wait.until(ec.visibility_of_element_located(*selector))
         obj = obj.text
